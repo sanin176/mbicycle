240bd0489b6304e2b384171a06efa297e6b19872
aleksandrzditovec <sanin17619@gmail.com>
2023-09-10 16:55:05 +0200

    first commit

---
+++ src/main/java/com/example/mbicycle/MbicycleApplication.java
@@ -0,0 +1,13 @@
+package com.example.mbicycle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MbicycleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MbicycleApplication.class, args);
+    }
+
+}
